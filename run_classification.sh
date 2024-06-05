ace169acd02a731aedb8d10f2f22559ed1bf2843
yanyifan <3200100917@zju.edu.cn>
2024-06-05 12:51:33 +0000

    update: hupr

--- run_classification.sh
+++ run_classification.sh
@@ -1,2 +1,2 @@
 export OMP_NUM_THREADS=10 
-python baselines/run_hupr_classification.py --version $1 --sampling_ratio 1 --config $1.yaml --gpuIDs '[0]' #--visDir /root/viz --eval 
+python baselines/run_hupr_classification.py --version $1 --sampling_ratio 1 --config $1.yaml --gpuIDs '[0, 1, 2, 3]' #--visDir /root/viz --eval 
